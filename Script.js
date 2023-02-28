169daecbfd2da379a22ca0166659caf159789545
Lucio Maldini <luciomaldini17@gmail.com>
2023-02-28 19:50:43 -0300

    Actualizacion del proyecto

---
+++ Script.js
@@ -0,0 +1,11 @@
+function mostrar(){
+    document.getElementById('mostrar').style.display = 'block';
+    document.getElementById('verMenos').style.display = 'block';
+    document.getElementById('verMas').style.display = 'none';
+}
+
+function ocultar(){
+    document.getElementById('mostrar').style.display = 'none';
+    document.getElementById('verMenos').style.display = 'none';
+    document.getElementById('verMas').style.display = 'block';
+}
